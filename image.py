02f549f3726c00c8cb075c3deb3be0a45a6585b8
RocketMan234 <39049432+RocketMan234@users.noreply.github.com>
2018-08-30 19:09:48 -0700

    Initial project files

---
+++ image.py
@@ -0,0 +1,19 @@
+from PIL import Image
+
+img = Image.open('sunset.jpeg')
+
+def fakeGrayScale(pixelTuple):
+    px = pixelTuple.load()
+    for i in range(pixelTuple.width):
+        for j in range(pixelTuple.height):
+            rgb = px[i, j]
+            r = rgb[0]
+            g = rgb[1]
+            b = rgb[2]
+            gray = int(((r + g + b)/3)* 1.5)
+            pixelTuple.putpixel((i, j), (0, 0, gray))
+    pixelTuple.show()
+    pixelTuple.save('blue.jpeg')
+
+
+fakeGrayScale(img)
